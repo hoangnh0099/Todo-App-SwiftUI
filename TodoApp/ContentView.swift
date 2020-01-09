8bcfa9a85f4e8b9483d5ddd0215b8edaea788982
Nguyen Huy Hoang <hoangnh0099@gmail.com>
2020-01-09 17:51:07 +0700

    Update

---
+++ TodoApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TodoApp
+//
+//  Created by Nguyễn Huy Hoàng on 1/9/20.
+//  Copyright © 2020 Nguyễn Huy Hoàng. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
